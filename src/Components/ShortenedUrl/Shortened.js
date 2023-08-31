f7e159976cf753497a2be5a34b2cd20296906c19
Marucha <marucha.webdev@yahoo.com>
2023-08-31 22:48:28 +0300

    style: Font sizes for small screens

--- src/Components/ShortenedUrl/Shortened.js
+++ src/Components/ShortenedUrl/Shortened.js
@@ -3,15 +3,17 @@ import { Button } from "../Buttons/Button"
 
 const ShortenedUrl = ({ original, shortened }) => {
 	return (
-		<div
-			className="flex mobile:flex-col max-mobile:flex-col bg-[#ffffff] rounded-md text-lg gap-2 text-left text-[14px] p-2
-			desktop:flex-row desktop:w-4/5 desktop:justify-between desktop:p-2 desktop:items-center shadow-2xl">
-			<p
-				className="text-dark-violet mobile:border-b-2 mobile:border-cyan mobile:border-b-1 max-mobile:border-cyan desktop:border-b-0">
-				{original.substring(0, 60)}...
-			</p>
-			<a className="text-cyan" href={shortened} target="_blank" rel="noreferrer">{shortened}</a>
-			<Button>Copy</Button>
+		<div className="">
+			<div
+				className="flex mobile:flex-col max-mobile:flex-col bg-[#ffffff] rounded-md text-lg gap-2 text-left text-[14px] p-3
+				desktop:flex-row desktop:w-4/5 desktop:justify-between desktop:p-2 desktop:items-center shadow-2xl max-mobile:text-sm mobile:text-sm">
+				<p
+					className="text-dark-violet mobile:border-b-2 mobile:border-cyan mobile:border-b-1 max-mobile:border-cyan desktop:border-b-0">
+					{original.substring(0, 60)}...
+				</p>
+				<a className="text-cyan" href={shortened} target="_blank" rel="noreferrer">{shortened}</a>
+				<Button>Copy</Button>
+			</div>
 		</div>
 	)
 }
